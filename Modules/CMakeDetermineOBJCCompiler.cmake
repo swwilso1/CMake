7fe89d7d5fe10a698ddf6c2ee1b35bbdac179282
Steve Wilson <stevew@wolfram.com>
2013-02-21 13:17:51 -0700

    Cherry pick of 0037afc and 757b491.

---
+++ Modules/CMakeDetermineOBJCCompiler.cmake
@@ -0,0 +1,108 @@
+
+
+#=============================================================================
+#
+# Copyright 2012 Wolfram Research Inc.
+#
+#=============================================================================
+
+# Determine the compiler to use for Objective-C programs
+
+# Sets the following variables:
+#	CMAKE_OBJC_COMPILER
+#	CMAKE_AR
+#	CMAKE_RANLIB
+
+if(NOT CMAKE_OBJC_COMPILER)
+	set(CMAKE_OBJC_COMPILER_INIT NOTFOUND)
+	
+	# Check for a compiler declared in the environment variable OBJC
+	if($ENV{OBJC} MATCHES ".+")
+		get_filename_component(CMAKE_OBJC_COMPILER_INIT $ENV{OBJC} PROGRAM PROGRAM_ARGS CMAKE_OBJC_FLAGS_ENV_INIT)
+		if(CMAKE_OBJC_FLAGS_ENV_INIT)
+			set(CMAKE_OBJC_COMPILER_ARG1 "${CMAKE_OBJC_FLAGS_ENV_INIT}" CACHE STRING "First argument to Objective-C compiler")
+		endif(CMAKE_OBJC_FLAGS_ENV_INIT)
+		if(NOT EXISTS ${CMAKE_OBJC_COMPILER_INIT})
+			message(FATAL_ERROR "Could not find compiler set in environment variable OBJC:\n$ENV{OBJC}.\n${CMAKE_OBJC_COMPILER_INIT}")
+		endif(NOT EXISTS ${CMAKE_OBJC_COMPILER_INIT})
+	endif($ENV{OBJC} MATCHES ".+")
+	
+	# List compilers to try:
+	if(CMAKE_OBJC_COMPILER_INIT)
+		set(CMAKE_OBJC_COMPILER_LIST ${CMAKE_OBJC_COMPILER_INIT})
+	else(CMAKE_OBJC_COMPILER_INIT)
+		set(CMAKE_OBJC_COMPILER_LIST gcc clang cc)
+	endif(CMAKE_OBJC_COMPILER_INIT)
+	
+	# Find the compiler.
+	find_program(CMAKE_OBJC_COMPILER NAMES ${CMAKE_OBJC_COMPILER_LIST} DOC "Objective-C compiler")
+	
+	if(CMAKE_OBJC_COMPILER_INIT AND NOT CMAKE_OBJC_COMPILER)
+		set(CMAKE_OBJC_COMPILER "${CMAKE_OBJC_CMPILER_INIT}" CACHE FILEPATH "Objective-C compiler" FORCE)
+	endif(CMAKE_OBJC_COMPILER_INIT AND NOT CMAKE_OBJC_COMPILER)
+
+endif(NOT CMAKE_OBJC_COMPILER)
+
+
+# To get to this part of the file, the user must have specified CMAKE_OBJC_COMPILER vial -D or a pre-made CMakeCache.txt
+#
+# If CMAKE_OBJC_COMPILER is a list of length 2, use the first item as CMAKE_OBJC_COMPILER 
+# and the second as CMAKE_OBJC_COMPILER_ARG1
+
+list(LENGTH CMAKE_OBJC_COMPILER _CMAKE_OBJC_COMPILER_LIST_LENGTH)
+if("${_CMAKE_OBJC_COMPILER_LIST_LENGTH}" EQUAL 2)
+	list(GET CMAKE_OBJC_COMPILER 1 CMAKE_OBJC_COMPILER_ARG1)
+	list(GET CMAKE_OBJC_COMPILER 0 CMAKE_OBJC_COMPILER)
+endif("${_CMAKE_OBJC_COMPILER_LIST_LENGTH}" EQUAL 2)
+
+# Check to see if the user specified a compiler without a path.
+# If found, put the value in the cache, if not do not overwrite with "NOTFOUND"
+get_filename_component(_CMAKE_USER_OBJC_COMPILER_PATH "${CMAKE_OBJC_COMPILER}" PATH)
+if(NOT _CMAKE_USER_OBJC_COMPILER_PATH)
+	find_program(CMAKE_OBJC_COMPILER_WITH_PATH NAMES ${CMAKE_OBJC_COMPILER})
+	mark_as_advanced(CMAKE_OBJC_COMPILER_WITH_PATH)
+	if(CMAKE_OBJC_COMPILER_WITH_PATH)
+		set(CMAKE_OBJC_COMPILER ${CMAKE_OBJC_COMPILER_WITH_PATH} CACHE STRING "Objective-C compiler" FORCE)
+	endif(CMAKE_OBJC_COMPILER_WITH_PATH)
+endif(NOT _CMAKE_USER_OBJC_COMPILER_PATH)
+mark_as_advanced(CMAKE_OBJC_COMPILER)
+
+if(NOT _CMAKE_TOOLCHAIN_LOCATION)
+	get_filename_component(_CMAKE_TOOLCHAIN_LOCATION "${CMAKE_OBJC_COMPILER}" PATH)
+endif(NOT _CMAKE_TOOLCHAIN_LOCATION)
+
+# Try to identify the compiler.
+SET(CMAKE_OBJC_COMPILER_ID)
+FILE(READ ${CMAKE_ROOT}/Modules/CMakePlatformId.h.in
+  CMAKE_OBJC_COMPILER_ID_PLATFORM_CONTENT)
+INCLUDE(${CMAKE_ROOT}/Modules/CMakeDetermineCompilerId.cmake)
+CMAKE_DETERMINE_COMPILER_ID(OBJC OBJCFLAGS CMakeOBJCCompilerId.m)
+
+
+# Look for libtool:
+if(NOT CMAKE_LIBTOOL)
+	find_program(CMAKE_LIBTOOL NAMES libtool PATHS "${_CMAKE_TOOLCHAIN_LOCATION}" DOC "Libtool")
+endif(NOT CMAKE_LIBTOOL)
+
+# Look for ar:
+if(NOT CMAKE_AR)
+	find_program(CMAKE_AR NAMES ar PATHS "${_CMAKE_TOOLCHAIN_LOCATION}" DOC "ar")
+endif(NOT CMAKE_AR)
+
+# Look for ranlib:
+if(NOT CMAKE_RANLIB)
+	find_program(CMAKE_RANLIB NAMES ranlib PATHS "${_CMAKE_TOOLCHAIN_LOCATION}" DOC "Ranlib tool")
+endif(NOT CMAKE_RANLIB)
+
+configure_file(${CMAKE_ROOT}/Modules/CMakeOBJCCompiler.cmake.in
+	${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOBJCCompiler.cmake
+	@ONLY IMMEDIATE
+)
+
+
+
+
+
+set(CMAKE_OBJC_COMPILER_ENV_VAR "OBJC")
+
+
---
+++ Modules/CMakeOBJCInformation.cmake
@@ -0,0 +1,127 @@
+
+#=============================================================================
+#
+# Copyright 2012 Wolfram Research Inc.
+#
+#=============================================================================
+
+if(UNIX)
+	set(CMAKE_OBJC_OUTPUT_EXTENSION .o)
+endif(UNIX)
+
+# Load compiler-specific information
+if(CMAKE_OBJC_COMPILER_ID)
+	include(Compiler/${CMAKE_OBJC_COMPILER_ID}-OBJC OPTIONAL)
+endif(CMAKE_OBJC_COMPILER_ID)
+
+set(CMAKE_BASE_NAME)
+get_filename_component(CMAKE_BASE_NAME ${CMAKE_OBJC_COMPILER} NAME_WE)
+
+# Load system- and compiler specific files
+if(CMAKE_OBJC_COMPILER_ID)
+	include(Platform/${CMAKE_SYSTEM_NAME}-${CMAKE_OBJC_COMPILER_ID}-OBJC OPTIONAL RESULT_VARIABLE _INCLUDED_FILE)
+endif(CMAKE_OBJC_COMPILER_ID)
+
+if(NOT _INCLUDED_FILE)
+	include(Platform/${CMAKE_SYSTEM_NAME}-${CMAKE_BASE_NAME} OPTIONAL RESULT_VARIABLE _INCLUDED_FILE)
+endif(NOT _INCLUDED_FILE)
+
+if(NOT _INCLUDED_FILE)
+	include(Platform/${CMAKE_SYSTEM_NAME} OPTIONAL)
+endif(NOT _INCLUDED_FILE)
+
+# add the flags to the cache based
+# on the initial values computed in the platform/*.cmake files
+# use _INIT variables so that this only happens the first time
+# and you can set these flags in the cmake cache
+set(CMAKE_OBJC_FLAGS_INIT "$ENV{OBJCFLAGS} ${CMAKE_OBJC_FLAGS_INIT}")
+# avoid just having a space as the initial value for the cache 
+if(CMAKE_OBJC_FLAGS_INIT STREQUAL " ")
+	set(CMAKE_OBJC_FLAGS_INIT)
+endif(CMAKE_OBJC_FLAGS_INIT STREQUAL " ")
+set(CMAKE_OBJC_FLAGS "${CMAKE_OBJC_FLAGS_INIT}" CACHE STRING
+     "Flags used by the compiler during all build types.")
+
+IF(NOT CMAKE_NOT_USING_CONFIG_FLAGS)
+  SET (CMAKE_OBJC_FLAGS_DEBUG "${CMAKE_OBJC_FLAGS_DEBUG_INIT}" CACHE STRING
+     "Flags used by the compiler during debug builds.")
+  SET (CMAKE_OBJC_FLAGS_MINSIZEREL "${CMAKE_OBJC_FLAGS_MINSIZEREL_INIT}" CACHE STRING
+      "Flags used by the compiler during release minsize builds.")
+  SET (CMAKE_OBJC_FLAGS_RELEASE "${CMAKE_OBJC_FLAGS_RELEASE_INIT}" CACHE STRING
+     "Flags used by the compiler during release builds.")
+  SET (CMAKE_OBJC_FLAGS_RELWITHDEBINFO "${CMAKE_OBJC_FLAGS_RELWITHDEBINFO_INIT}" CACHE STRING
+     "Flags used by the compiler during Release with Debug Info builds.")
+
+ENDIF(NOT CMAKE_NOT_USING_CONFIG_FLAGS)
+
+
+# Define the following rules:
+# CMAKE_OBJC_CREATE_SHARED_LIBRARY
+# CMAKE_OBJC_CREATE_SHARED_MODULE
+# CMAKE_OBJC_COMPILE_OBJECT
+# CMAKE_OBJC_LINK_EXECUTABLE
+
+# Variables supplied by the generator at use time
+# <TARGET>
+# <TARGET_BASE> the target without the suffix
+# <OBJECTS>
+# <OBJECT>
+# <LINK_LIBRARIES>
+# <FLAGS>
+# <LINK_FLAGS>
+
+# Objective-C compiler information
+# <CMAKE_OBJC_COMPILER>
+# <CMAKE_SHARED_LIBRARY_CREATE_OBJC_FLAGS>
+# <CMAKE_OBJC_SHARED_MODULE_CREATE_FLAGS>
+# <CMAKE_OBJC_LINK_FLAGS>
+
+# Static library tools
+# <CMAKE_AR>
+# <CMAKE_RANLIB>
+
+# Create a shared Objective-C library
+if(NOT CMAKE_OBJC_CREATE_SHARED_LIBRARY)
+	set(CMAKE_OBJC_CREATE_SHARED_LIBRARY
+		"<CMAKE_AR> -dynamic -o <TARGET> <LINK_FLAGS> <OBJECTS>"
+	)
+endif(NOT CMAKE_OBJC_CREATE_SHARED_LIBRARY)
+
+
+# Create a shared module copy of the shared library rule by default
+if(NOT CMAKE_OBJC_CREATE_SHARED_MODULE)
+	set(CMAKE_OBJC_CREATE_SHARED_MODULE ${CMAKE_OBJC_CREATE_SHARED_MODULE})
+endif(NOT CMAKE_OBJC_CREATE_SHARED_MODULE)
+
+set(CMAKE_OBJC_ARCHIVE_CREATE "<CMAKE_LIBTOOL> -static -o <TARGET> <LINK_FLAGS> <OBJECTS>")
+set(CMAKE_OBJC_ARCHIVE_APPEND "<CMAKE_LIBTOOL> -static -o <TARGET> <LINK_FLAGS> <TARGET> <OBJECTS>")
+set(CMAKE_OBJC_ARCHIVE_FINISH "<CMAKE_RANLIB> <TARGET>")
+
+# Compile an Objective-C file into an object file
+if(NOT CMAKE_OBJC_COMPILE_OBJECT)
+	set(CMAKE_OBJC_COMPILE_OBJECT
+		"<CMAKE_OBJC_COMPILER> <DEFINES> <FLAGS> -o <OBJECT> -c <SOURCE>"
+	)
+endif(NOT CMAKE_OBJC_COMPILE_OBJECT)
+
+
+# Link an Objective-C program
+if(NOT CMAKE_OBJC_LINK_EXECUTABLE)
+	set(CMAKE_OBJC_LINK_EXECUTABLE
+		"<CMAKE_OBJC_COMPILER> <FLAGS> <CMAKE_OBJC_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>"
+	)
+endif(NOT CMAKE_OBJC_LINK_EXECUTABLE)
+
+mark_as_advanced(
+	CMAKE_BUILD_TOOL
+	CMAKE_VERBOSE_MAKEFILE
+	CMAKE_OBJC_FLAGS
+	CMAKE_OBJC_FLAGS_RELEASE
+	CMAKE_OBJC_FLAGS_RELWITHDEBINFO
+	CMAKE_OBJC_FLAGS_MINSIZEREL
+	CMAKE_OBJC_FLAGS_DEBUG
+)
+
+set(CMAKE_OBJC_INFORMATION_LOADED 1)
+
+
