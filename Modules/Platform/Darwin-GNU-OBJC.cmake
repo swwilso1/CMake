5b23162b83f905c52b750c89d61527acb3ae9bd6
Steve Wilson <stevew@wolfram.com>
2013-05-02 15:02:51 -0600

    Use cmake_gnu_set_sysroot_flag().

--- Modules/Platform/Darwin-GNU-OBJC.cmake
+++ Modules/Platform/Darwin-GNU-OBJC.cmake
@@ -1,6 +1,6 @@
 include(Platform/Darwin-GNU)
 __darwin_compiler_gnu(OBJC)
-cmake_gnu_has_isysroot(OBJC)
+cmake_gnu_set_sysroot_flag(OBJC)
 cmake_gnu_set_osx_deployment_target_flag(OBJC)
 
 set(CMAKE_OBJC_FLAGS_INIT "")
