d03c9410b9fe7cf6f453b24761bdc890cb0dbc7e
Steve Wilson <stevew@wolfram.com>
2013-12-12 13:24:04 -0700

    Remove strlcat definition from strequal.h.

--- Utilities/cmcurl/strequal.h
+++ Utilities/cmcurl/strequal.h
@@ -35,10 +35,6 @@
 /* case insensitive strstr() */
 char *Curl_strcasestr(const char *haystack, const char *needle);
 
-#ifndef HAVE_STRLCAT
-#define strlcat(x,y,z) Curl_strlcat(x,y,z)
-#endif
-
 #endif
 
 
