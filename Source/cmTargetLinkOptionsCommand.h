a172180054949aefd4540afdfecd6f5fd29badd7
Steve Wilson <stevew@wolfram.com>
2014-07-15 11:03:27 -0600

    Tests: Update tests for target_link_options.

--- Source/cmTargetLinkOptionsCommand.h
+++ Source/cmTargetLinkOptionsCommand.h
@@ -36,7 +36,7 @@ public:
   /**
    * The name of the command as specified in CMakeList.txt.
    */
-  virtual const char* GetName() const { return "target_link_options";}
+  virtual std::string GetName() const { return "target_link_options";}
 
   cmTypeMacro(cmTargetLinkOptionsCommand, cmTargetPropCommandBase);
 
@@ -44,7 +44,7 @@ private:
   virtual void HandleImportedTarget(const std::string &tgt);
   virtual void HandleMissingTarget(const std::string &name);
 
-  virtual void HandleDirectContent(cmTarget *tgt,
+  virtual bool HandleDirectContent(cmTarget *tgt,
                                    const std::vector<std::string> &content,
                                    bool prepend, bool system);
   virtual std::string Join(const std::vector<std::string> &content);
