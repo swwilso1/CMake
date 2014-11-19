bdce9e7614cab215fb70f4376091a5a22e391204
Steve Wilson <steve.wilson@savantsystems.com>
2014-11-18 21:11:21 -0500

    Fix bug in test_compile and CMAKE_LANG_FLAGS.

--- Source/cmCoreTryCompile.cxx
+++ Source/cmCoreTryCompile.cxx
@@ -325,8 +325,11 @@ int cmCoreTryCompile::TryCompileCode(std::vector<std::string> const& argv)
       {
       std::string langFlags = "CMAKE_" + *li + "_FLAGS";
       const char* flags = this->Makefile->GetDefinition(langFlags.c_str());
-      fprintf(fout, "set(CMAKE_%s_FLAGS %s)\n", li->c_str(),
-              lg->EscapeForCMake(flags?flags:"").c_str());
+      if(flags && strlen(flags) > 0)
+        {
+        fprintf(fout, "set(CMAKE_%s_FLAGS %s)\n", li->c_str(),
+                lg->EscapeForCMake(flags?flags:"").c_str());
+        }
       fprintf(fout, "set(CMAKE_%s_FLAGS \"${CMAKE_%s_FLAGS}"
               " ${COMPILE_DEFINITIONS}\")\n", li->c_str(), li->c_str());
       }
